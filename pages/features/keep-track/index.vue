9460b7ec9bfbd2ddff9675ec237c0e00cb448253
Marije Pierson <marijepierson@gmail.com>
2018-05-25 18:41:52 +0200

    more more more

---
+++ pages/features/keep-track/index.vue
@@ -0,0 +1,115 @@
+<template>
+<div class='page-container' >
+    <template v-for='(block, index) in blocks'  >
+      <v-container v-if='block.inContainer' :key='index' >
+        <component :is='block.name' v-bind='block.props' :class='block.classes' />
+      </v-container>
+      <component v-else :is='block.name' v-bind='block.props' :key='index' :class='block.classes' />
+    </template>
+</div>
+</template>
+
+<script>
+import HeroInfo from '@/components/HeroInfo'
+import FeatureBox from '@/components/FeatureBox'
+import FeatureBlock from '@/components/FeatureBlock'
+import Callout from '@/components/Callout'
+import HeroParallax from '@/components/HeroParallax'
+
+export default {
+  name: 'Keep track',
+  components: {FeatureBox, FeatureBlock, Callout, HeroParallax, HeroInfo},
+  head () {
+    return {
+      title: 'Manage your appointments efficiently | AppointmentGuru',
+      meta: [
+        { hid: 'description', name: 'description', content: 'AppointmentGuru appointment management give you: appointment notifications, an online shared calendar, mobile app for managing on the go and more' }
+      ]
+    }
+  },
+  data () {
+    return {
+      campaignParts: {
+        utmSource: 'website',
+        utmMedium: 'landing-page',
+        utmCampaign: ''
+      },
+      blocks: [
+        {
+          name: 'HeroInfo',
+          inContainer: true,
+          props: {
+            classes: 'mt-4',
+            tagline: 'High-level reporting gives insight into your business',
+            col1Headline: 'For your business',
+            col2Headline: 'For your clients',
+            col1Content: 'Understand key aspects and keep an eye on your overall financial status.',
+            col2Content: 'Not much, unless you want to show them!'
+          }
+        },
+        {
+          name: 'Callout',
+          props: {
+            callout: 'Spend less time dealing with your appointment admin and free up more time for the important things.'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'View reporting over different date ranges',
+            content: 'Generate a report for a week, a calendar month, the whole year.',
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            tagline: "Check on your cashflow",
+            content: "Quickly see how much money you've earned, what's been paid and the total amount still outstanding.",
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'Which appointment types are more popular?',
+            content: 'Graphs break down the appointment types and their numbers.',
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            tagline: 'Which days and times are the busiest?',
+            content: "This kind of insight can be valuable when making decisions about when to best serve your clients.",
+            img: '/placeholder.png'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@media only screen and (min-width: 1904px) {
+  .container {
+    max-width: 1185px !important;
+  }
+}
+@media only screen and (max-width: 599px) {
+  .container {
+      padding: 0px;
+  }
+}
+.blockquote.smaller { font-size: 16px; }
+
+</style>
