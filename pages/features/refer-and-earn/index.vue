ad5f5de600a7a1fa44fc5d3f4049af57cff63768
Marije Pierson <marijepierson@gmail.com>
2018-05-28 16:22:30 +0200

    /features/refer

---
+++ pages/features/refer-and-earn/index.vue
@@ -0,0 +1,86 @@
+<template>
+<div class='page-container' >
+    <template v-for='(block, index) in blocks'  >
+      <v-container v-if='block.inContainer' :key='index' >
+        <component :is='block.name' v-bind='block.props' :class='block.classes' />
+      </v-container>
+      <component v-else :is='block.name' v-bind='block.props' :key='index' :class='block.classes' />
+    </template>
+</div>
+</template>
+
+<script>
+import HeroInfo from '@/components/HeroInfo'
+import FeatureBox from '@/components/FeatureBox'
+import FeatureBlock from '@/components/FeatureBlock'
+import Callout from '@/components/Callout'
+import HeroParallax from '@/components/HeroParallax'
+
+export default {
+  name: 'HomePage',
+  components: {FeatureBox, FeatureBlock, Callout, HeroParallax, HeroInfo},
+  head () {
+    return {
+      title: 'Manage your appointments efficiently | AppointmentGuru',
+      meta: [
+        { hid: 'description', name: 'description', content: 'AppointmentGuru appointment management give you: appointment notifications, an online shared calendar, mobile app for managing on the go and more' }
+      ]
+    }
+  },
+  data () {
+    return {
+      campaignParts: {
+        utmSource: 'website',
+        utmMedium: 'landing-page',
+        utmCampaign: ''
+      },
+      blocks: [
+        {
+          name: 'HeroInfo',
+          inContainer: true,
+          props: {
+            classes: 'mt-4',
+            tagline: "Refer AppointmentGuru and you'll get rewarded",
+            col1Headline: 'For your business',
+            col2Headline: 'For your clients',
+            col1Content: 'A free month for each successful referral!',
+            col2Content: 'Access to more practitioners on the AppointmentGuru platform'
+          }
+        },
+        {
+          name: 'Callout',
+          props: {
+            callout: 'Spend less time dealing with your appointment admin and free up more time for the important things.'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'Refer AppointmentGuru to your network',
+            content: "For each successful referral (your referral becomes a paying AppointmentGuru customer), we'll give you a free month on your AppointmentGuru subscription.",
+            img: '/placeholder.png'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@media only screen and (min-width: 1904px) {
+  .container {
+    max-width: 1185px !important;
+  }
+}
+@media only screen and (max-width: 599px) {
+  .container {
+      padding: 0px;
+  }
+}
+.blockquote.smaller { font-size: 16px; }
+
+</style>
