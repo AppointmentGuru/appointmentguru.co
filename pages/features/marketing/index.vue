cb543e9e95102a3da44819e427dad74fbbc37f19
Marije Pierson <marijepierson@gmail.com>
2018-05-28 16:16:01 +0200

    /features/marketing and /training

---
+++ pages/features/marketing/index.vue
@@ -0,0 +1,115 @@
+<template>
+<div class='page-container' >
+    <template v-for='(block, index) in blocks'  >
+      <v-container v-if='block.inContainer' :key='index' >
+        <component :is='block.name' v-bind='block.props' :class='block.classes' />
+      </v-container>
+      <component v-else :is='block.name' v-bind='block.props' :key='index' :class='block.classes' />
+    </template>
+</div>
+</template>
+
+<script>
+import HeroInfo from '@/components/HeroInfo'
+import FeatureBox from '@/components/FeatureBox'
+import FeatureBlock from '@/components/FeatureBlock'
+import Callout from '@/components/Callout'
+import HeroParallax from '@/components/HeroParallax'
+
+export default {
+  name: 'HomePage',
+  components: {FeatureBox, FeatureBlock, Callout, HeroParallax, HeroInfo},
+  head () {
+    return {
+      title: 'Manage your appointments efficiently | AppointmentGuru',
+      meta: [
+        { hid: 'description', name: 'description', content: 'AppointmentGuru appointment management give you: appointment notifications, an online shared calendar, mobile app for managing on the go and more' }
+      ]
+    }
+  },
+  data () {
+    return {
+      campaignParts: {
+        utmSource: 'website',
+        utmMedium: 'landing-page',
+        utmCampaign: ''
+      },
+      blocks: [
+        {
+          name: 'HeroInfo',
+          inContainer: true,
+          props: {
+            classes: 'mt-4',
+            tagline: 'AppointmentGuru provides marketing material that includes your branding',
+            col1Headline: 'For your business',
+            col2Headline: 'For your clients',
+            col1Content: 'Brand awareness and more options to reach your customers',
+            col2Content: 'Find out about your business and keep informed'
+          }
+        },
+        {
+          name: 'Callout',
+          props: {
+            callout: 'Spend less time dealing with your appointment admin and free up more time for the important things.'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'A free website',
+            content: 'Your AppointmentGuru account includes a free website so that your clients can make appointments and get more information on your business.',
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            tagline: 'Table talkers',
+            content: "A free table talker, displaying a QR code for your clients to scan with their phone. It'll open your website, prompt them to download the AppointmentGuru app or open your listing in the AppointmentGuru app. Making it easy for them to find information on your business and make an appointment with you.",
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'Business cards',
+            content: 'Business cards displaying your contact information and QR code (with the same functionality as the table talkers) to take bookings. The first 100 are a gift from us!',
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            tagline: 'Free listing in the AppointmentGuru app directory',
+            content: 'An optional, free listing in the AppointmentGuru app directory. Clients will be able to book and manage appointments with you.',
+            img: '/placeholder.png'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@media only screen and (min-width: 1904px) {
+  .container {
+    max-width: 1185px !important;
+  }
+}
+@media only screen and (max-width: 599px) {
+  .container {
+      padding: 0px;
+  }
+}
+.blockquote.smaller { font-size: 16px; }
+
+</style>
---
+++ pages/features/training-and-support/index.vue
@@ -0,0 +1,115 @@
+<template>
+<div class='page-container' >
+    <template v-for='(block, index) in blocks'  >
+      <v-container v-if='block.inContainer' :key='index' >
+        <component :is='block.name' v-bind='block.props' :class='block.classes' />
+      </v-container>
+      <component v-else :is='block.name' v-bind='block.props' :key='index' :class='block.classes' />
+    </template>
+</div>
+</template>
+
+<script>
+import HeroInfo from '@/components/HeroInfo'
+import FeatureBox from '@/components/FeatureBox'
+import FeatureBlock from '@/components/FeatureBlock'
+import Callout from '@/components/Callout'
+import HeroParallax from '@/components/HeroParallax'
+
+export default {
+  name: 'HomePage',
+  components: {FeatureBox, FeatureBlock, Callout, HeroParallax, HeroInfo},
+  head () {
+    return {
+      title: 'Manage your appointments efficiently | AppointmentGuru',
+      meta: [
+        { hid: 'description', name: 'description', content: 'AppointmentGuru appointment management give you: appointment notifications, an online shared calendar, mobile app for managing on the go and more' }
+      ]
+    }
+  },
+  data () {
+    return {
+      campaignParts: {
+        utmSource: 'website',
+        utmMedium: 'landing-page',
+        utmCampaign: ''
+      },
+      blocks: [
+        {
+          name: 'HeroInfo',
+          inContainer: true,
+          props: {
+            classes: 'mt-4',
+            tagline: "We want you to succeed and we're here with free training and support!",
+            col1Headline: 'For your business',
+            col2Headline: 'For your clients',
+            col1Content: 'All the help you need to get the most out of AppointmentGuru',
+            col2Content: 'Just more awesomeness!'
+          }
+        },
+        {
+          name: 'Callout',
+          props: {
+            callout: 'Spend less time dealing with your appointment admin and free up more time for the important things.'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'Account setup',
+            content: "Setting up an account with a new service can be time-consuming - we're here to help every step of the way.",
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            tagline: 'Training',
+            content: "If you're located in Joburg or Cape Town, we'll come and give you some one-on-one training. Or, online video training!",
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'Support',
+            content: "Live chat on our website or in the app - we're available almost all of the time. And lots of useful information in the help section of our website.",
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            tagline: 'Quick how-to videos',
+            content: 'If you prefer to watch a video, our Youtube channel has lots of quick how-to videos to help get your AppointmentGuru account up-and-running.',
+            img: '/placeholder.png'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@media only screen and (min-width: 1904px) {
+  .container {
+    max-width: 1185px !important;
+  }
+}
+@media only screen and (max-width: 599px) {
+  .container {
+      padding: 0px;
+  }
+}
+.blockquote.smaller { font-size: 16px; }
+
+</style>
