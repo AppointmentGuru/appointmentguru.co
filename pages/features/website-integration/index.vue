26deacf10bfc29afa3399c7d9de6494381d7422f
Marije Pierson <marijepierson@gmail.com>
2018-05-28 15:47:25 +0200

    add /features/website integration

---
+++ pages/features/website-integration/index.vue
@@ -0,0 +1,106 @@
+<template>
+<div class='page-container' >
+    <template v-for='(block, index) in blocks'  >
+      <v-container v-if='block.inContainer' :key='index' >
+        <component :is='block.name' v-bind='block.props' :class='block.classes' />
+      </v-container>
+      <component v-else :is='block.name' v-bind='block.props' :key='index' :class='block.classes' />
+    </template>
+</div>
+</template>
+
+<script>
+import HeroInfo from '@/components/HeroInfo'
+import FeatureBox from '@/components/FeatureBox'
+import FeatureBlock from '@/components/FeatureBlock'
+import Callout from '@/components/Callout'
+import HeroParallax from '@/components/HeroParallax'
+
+export default {
+  name: 'HomePage',
+  components: {FeatureBox, FeatureBlock, Callout, HeroParallax, HeroInfo},
+  head () {
+    return {
+      title: 'Manage your appointments efficiently | AppointmentGuru',
+      meta: [
+        { hid: 'description', name: 'description', content: 'AppointmentGuru appointment management give you: appointment notifications, an online shared calendar, mobile app for managing on the go and more' }
+      ]
+    }
+  },
+  data () {
+    return {
+      campaignParts: {
+        utmSource: 'website',
+        utmMedium: 'landing-page',
+        utmCampaign: ''
+      },
+      blocks: [
+        {
+          name: 'HeroInfo',
+          inContainer: true,
+          props: {
+            classes: 'mt-4',
+            tagline: 'Get online and accept appointments',
+            col1Headline: 'For your business',
+            col2Headline: 'For your clients',
+            col1Content: 'An online presence and available for anytime-appointment booking',
+            col2Content: 'Make appointments anytime and also get info on your business'
+          }
+        },
+        {
+          name: 'Callout',
+          props: {
+            callout: 'Spend less time dealing with your appointment admin and free up more time for the important things.'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'Free AppointmentGuru website',
+            content: 'A one-page website with information on your business and a place to accept appointments.',
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            tagline: 'List in the AppointmentGuru app',
+            content: 'Clients can download the AppointmentGuru app to make and manage their appointments with you.',
+            img: '/placeholder.png'
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: 'mt-4',
+            isLeft: true,
+            withBackground: true,
+            tagline: 'Add AppointmentGuru to your website',
+            content: 'Add the AppointmentGuru booking code to your existing website so that appointments can be made.',
+            img: '/placeholder.png'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@media only screen and (min-width: 1904px) {
+  .container {
+    max-width: 1185px !important;
+  }
+}
+@media only screen and (max-width: 599px) {
+  .container {
+      padding: 0px;
+  }
+}
+.blockquote.smaller { font-size: 16px; }
+
+</style>
