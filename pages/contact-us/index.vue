353d11ccb3ffe739150f403ca6c82b5f7104450a
Christo Crampton <info@38.co.za>
2020-04-03 05:35:45 +0200

    Adds zoom verify

--- pages/contact-us/index.vue
+++ pages/contact-us/index.vue
@@ -1,7 +1,13 @@
 <template  >
   <v-container class='page-container' >
-    <h1>Submit a support request</h1>
-    <iframe title="Feedback Form" class="freshwidget-embedded-form" id="freshwidget-embedded-form" src="https://appointmentguru.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&submitTitle=Submit&submitThanks=Thank-you+for+contacting+support.+You+should+expect+a+reply+within+24+hours.+Please+feel+free+to+browse+our+help+documentation+at%3A+https%3A%2F%2Fappointmentguru.co%2Fhelp%2F+in+the+meantime%0D%0A%0D%0A&screenshot=No&searchArea=no" scrolling="no" height="500px" width="100%" frameborder="0" >
+
+
+    <h2>Submit a support request</h2>
+    <iframe
+      title="Feedback Form"
+      class="freshwidget-embedded-form"
+      id="freshwidget-embedded-form"
+      src="https://appointmentguru.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&submitTitle=Submit&submitThanks=Thank-you+for+contacting+support.+You+should+expect+a+reply+within+24+hours.+Please+feel+free+to+browse+our+help+documentation+at%3A+https%3A%2F%2Fappointmentguru.co%2Fhelp%2F+in+the+meantime%0D%0A%0D%0A&screenshot=No&searchArea=no" scrolling="no" height="500px" width="100%" frameborder="0" >
     </iframe>
 
   </v-container>
