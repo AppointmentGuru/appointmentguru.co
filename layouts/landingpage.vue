c2b79d93db7678536c8ef014fc3aa4de341bd4e7
Christo Crampton <info@38.co.za>
2020-04-20 12:30:25 -0500

    Adds Zoom LP

---
+++ layouts/landingpage.vue
@@ -0,0 +1,220 @@
+<template>
+  <v-app light>
+     <v-navigation-drawer app disable-resize-watcher dark :value='showNav' >
+      <v-toolbar app dark color='orange' >
+        <nuxt-link to='/' >
+          <img src='~/assets/icon.png' width='40px' height='40px' />
+        </nuxt-link>
+        <v-toolbar-title color='orange' >
+          <nuxt-link to='/' style='text-decoration:none;color:white;' >
+          Appointment<span style='color:black;' >Guru</span></nuxt-link>
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-list class='page-container'  >
+        <v-subheader>Navigation</v-subheader>
+        <v-divider></v-divider>
+        <v-list-tile to='/features' >
+          <v-list-tile-title >Features</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile to='/pricing' >
+          <v-list-tile-title >Pricing</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile to='/help' >
+          <v-list-tile-title >Help</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile to='/blog'>
+          <v-list-tile-title >Blog</v-list-tile-title>
+        </v-list-tile>
+        <v-divider></v-divider>
+        <v-list-tile href='https://app.appointmentguru.co' target='_blank' >
+          <v-list-tile-title >Login</v-list-tile-title>
+        </v-list-tile>
+        <!-- <v-list-tile @click='showsignup = true' >
+          <v-list-tile-title >Sign up</v-list-tile-title>
+        </v-list-tile> -->
+      </v-list>
+     </v-navigation-drawer>
+    <v-toolbar app dark color='orange' >
+      <nuxt-link to='/' >
+        <img src='~/assets/icon.png' width='40px' height='40px' />
+      </nuxt-link>
+      <v-toolbar-title color='orange' class='mr-4' >
+        <nuxt-link to='/' style='text-decoration:none;color:white;' >
+        Appointment<span style='color:black;'>Guru</span></nuxt-link>
+      </v-toolbar-title>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn to='/features/' flat>Features</v-btn>
+        <v-btn to='/pricing/' flat>Pricing</v-btn>
+        <v-btn to='/help/' flat>Help</v-btn>
+        <v-btn to='/blog/' flat>Blog</v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn href='https://app.appointmentguru.co' flat>Login</v-btn>
+        <!-- <v-btn @click='showsignup = true' color='green' class='elevation-0' >Sign up</v-btn> -->
+      </v-toolbar-items>
+      <v-toolbar-side-icon class='hidden-md-and-up' @click='showNav = !showNav' ></v-toolbar-side-icon>
+    </v-toolbar>
+
+    <nuxt/>
+
+    <footer class='page-footer callout mt-4' >
+      <v-container >
+      <v-layout wrap>
+        <v-flex sm5 xs12 >
+          <section class='pa-3' >
+            <blockquote class='blockquote smaller ml-1 pl-0'>
+              AppointmentGuru is a South African start-up based in Johannesburg and Cape Town. We're passionate about helping small businesses succeed by taking advantage of technology, empowering them to do more with less.
+            </blockquote>
+          </section>
+          <section class='pa-3' >
+            <h4 class='subheading' >MOBILE APP FOR CLIENTS</h4>
+            <blockquote class='blockquote smaller ml-1 pl-0'>
+              Clients of AppointmentGuru practitioners can book and manage their appointments from our mobile app.
+            </blockquote>
+          </section>
+          <section>
+            <div >
+              <a class="app-link" id="appleLink"
+                href="https://itunes.apple.com/us/app/appointment-guru/id1163805838?mt=8">
+                <img class="ml-3 app-icon img-responsive" src="https://getapp.guru/template/badgeappstore.png">
+              </a>
+              <a class="app-link" id="googleLink" href="https://play.google.com/store/apps/details?id=za.co.AppointmentGuru&amp;utm_source=getapp.guru&amp;pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+                <img class="ml-3 app-icon img-responsive" src="https://getapp.guru/template/badgegoogleplay.png">
+              </a>
+            </div>
+          </section>
+        </v-flex>
+        <v-flex sm7 xs12 >
+          <v-layout class='mt-3' row wrap >
+            <v-flex xs6 sm4 class='text-sm-right pa-3'>
+            <h4 class='subheading uppercase' >About us</h4>
+              <ul class="list-unstyled list-spaced mt-3 ">
+                <li class="text-muted"><a href="/features/">Features</a></li>
+                <li class="text-muted"><a href="/pricing/">Pricing</a></li>
+                <li class="text-muted"><a href="/features/apps/">Apps</a></li>
+                <li class="text-muted"><a href="https://www.youtube.com/channel/UCAZppp_Yg3dC9M1-ec8E6Tg">Watch our videos</a></li>
+              </ul>
+            </v-flex>
+            <v-flex xs6 sm4 class='text-sm-right pa-3'>
+            <h4 class='subheading uppercase' >Information</h4>
+              <ul class="list-unstyled list-spaced mt-3">
+                <li class="text-muted"><a href="/blog/">Blog</a></li>
+                <li class="text-muted"><a href="/help/">Help</a></li>
+                <li class="text-muted"><a href="/terms/">Terms</a> & <a href="/privacy/">Privacy</a></li>
+                <li class="text-muted"><a href="https://appointmentguru.freshstatus.io">System Status</a></li>
+               </ul>
+            </v-flex>
+            <v-flex xs6 sm4 class='text-sm-right pa-3' >
+              <h4 class='subheading uppercase' >Connect</h4>
+              <ul class="list-unstyled list-spaced mt-3">
+              <li class="text-muted"><a href="/contact-us">Contact us</a></li>
+              <li class="text-muted"><a href="mailto:support@appointmentguru.co">Email us</a></li>
+              <li class="text-muted"><a href="https://www.facebook.com/appointmentguru/">Facebook</a></li>
+              <li class="text-muted"><a href="https://www.instagram.com/appointmentguru/">Instagram</a></li>
+              <li class="text-muted"><a href="/get-a-guru">Book a demo</a></li>
+              </ul>
+            </v-flex>
+          </v-layout>
+          <v-layout class='text-xs-right' >
+            <v-spacer></v-spacer>
+            <img class='mt-4' align='right' src='~/assets/logo-full-dark.png' />
+          </v-layout>
+        </v-flex>
+      </v-layout>
+      </v-container>
+      <v-divider dark></v-divider>
+      <v-footer dark class='pl-3 pr-3'>
+        <v-spacer></v-spacer>
+        &copy; AppointmentGuru {{ new Date().getFullYear() }}</v-footer>
+    </footer>
+
+  </v-app>
+</template>
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      showNav: false,
+      showsignup: false
+    }
+  },
+  mounted () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const referrer = urlParams.get('utm_campaign');
+    let referrers = this.$cookies.get('referrers')
+    if (!referrers) { referrers = [] }
+    if (referrer && referrers.indexOf(referrer) === -1) {
+      referrers.push(referrer)
+      let host = window.location.host
+      console.log(host)
+      let opts = {
+        "domain": host,
+        "path": "/"
+      }
+      this.$cookies.set('referrers', referrers, opts)
+    }
+  },
+  computed: {
+    isMobile () {
+      return this.$vuetify.breakpoint.smAndDown
+    },
+    signupDialog () {
+      return this.$store.state.showSignupDialog
+    }
+  },
+  watch: {
+    signupDialog () {
+      this.showsignup = true
+      this.$ga.event({eventCategory: 'signup', eventAction: 'startSignup'})
+    }
+  },
+  head: {
+    script: [
+      {
+        src: 'https://get-started.netlify.com/static/js/app.js',
+        // src: 'https://s3.eu-central-1.amazonaws.com/static.appointmentguru.co/signup/static/js/app.js',
+        body: true,
+        defer: true
+      }
+    ]
+  }
+}
+</script>
+<style>
+@media only screen and (min-width: 1264px) {
+  .container {
+    max-width: 900px !important;
+  }
+}
+.page-container {
+  margin-top:60px;
+}
+.list-unstyled {list-style: none;}
+.list-unstyled li a {
+  color: white !important;
+  text-decoration: none;
+}
+.uppercase { text-transform: uppercase; }
+.callout {
+  background-color:#262f36;color:white;
+  border-top: solid 4px #E65100;
+}
+.app-icon { max-width: 140px; }
+.img-responsive{ width: 100%; }
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: height .5s
+}
+.slide-enter, .slide-leave-to /* .fade-leave-active in <2.1.8 */ {
+  height: 0px;
+}
+</style>
---
+++ pages/lp/zoom/index.vue
@@ -0,0 +1,250 @@
+<template>
+  <div>
+    <v-container style="min-height:300px;">
+      <v-layout row class="mt-4 mb-3">
+        <v-flex xs7 class="text-center">
+          <!-- <v-img class="white--text align-end" height="200px" src="/img/lp/zoom/video_call.png" /> -->
+          <!-- <v-card-media
+              class="white--text align-end"
+              height="300px"
+              src="/img/lp/zoom/video_call.png"
+            >
+              <v-card-title
+                class="subheading white--text pl-5"
+              >Add Zoom to your AppointmentGuru account</v-card-title>
+          </v-card-media>-->
+          <div
+            style="background-image:url('/img/lp/zoom/video_call.png');height:400px;background-position:bottom left;"
+            class="text-center elevation-2 light-blue lighten-5"
+          >
+            <!-- <img src="~/static/img/lp/zoom/video_call.png" /> -->
+            <center>
+              <p class="title pt-5">Already an AppointmentGuru member?</p>
+              <v-btn
+                href="https://api.appointmentguru.co/integrations/zoom/"
+                large
+                color="primary"
+              >Add the Zoom integration now</v-btn>
+            </center>
+          </div>
+        </v-flex>
+        <v-flex xs5>
+          <v-card class="my-4 mt-5 ml-3 elevation-10">
+            <v-list>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>New to AppointmentGuru?</v-list-tile-title>
+                  <v-list-tile-sub-title>Start your free trial</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+            <v-divider></v-divider>
+            <!-- <blockquote class="blockquote">Create your AppointmentGuru now</blockquote> -->
+            <no-ssr placeholder="Loading...">
+              <div id="agsignup"></div>
+            </no-ssr>
+            <!-- <v-card-actions>
+              Already got an AppointmentGuru account?
+              <a href>connect Zoom now</a>
+            </v-card-actions>-->
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <template v-for="(block, index) in blocks">
+      <v-container v-if="block.inContainer" :key="index">
+        <component :is="block.name" v-bind="block.props" :class="block.classes" />
+      </v-container>
+      <component v-else :is="block.name" v-bind="block.props" :key="index" :class="block.classes" />
+    </template>
+  </div>
+</template>
+
+<script>
+import FeatureBox from "@/components/FeatureBox";
+import FeatureBlock from "@/components/FeatureBlock";
+import Callout from "@/components/Callout";
+import HeroParallax from "@/components/HeroParallax";
+import Testimonial from "@/components/Testimonial";
+import VideoHeroParallax from "@/components/VideoHeroParallax";
+
+export default {
+  name: "LandingPage",
+  layout: "landingpage",
+  components: {
+    FeatureBox,
+    FeatureBlock,
+    Callout,
+    HeroParallax,
+    VideoHeroParallax,
+    Testimonial
+  },
+  head() {
+    return {
+      title: "AppointmentGuru - Be better at admin, spend less time doing it",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "AppointmentGuru helps you to automate appointments, invoices and customer management"
+        }
+      ]
+    };
+  },
+  data() {
+    return {
+      heroBackground:
+        "https://images.unsplash.com/photo-1517497052582-25e6fe8ec001?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a796c72fc4b68772c227b39dc6c3754&auto=format&fit=crop&w=1950&q=80",
+      blocks: [
+        // {
+        //   name: 'VideoHeroParallax',
+        //   props: {
+        //     headline: 'Be better at admin. Spend less time doing it.',
+        //     tagline: 'Appointments. Invoices. Clients. More.',
+        //     video: 'https://www.youtube-nocookie.com/embed/S-RBfTBraWc?rel=0',
+        //     backgroundImage: 'https://images.unsplash.com/photo-1517497052582-25e6fe8ec001?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a796c72fc4b68772c227b39dc6c3754&auto=format&fit=crop&w=1950&q=80'
+        //   }
+        // },
+        // {
+        //   name: 'HeroParallax',
+        //   props: {
+        //     headline: 'Be better at admin.<br/>Spend less time doing it.',
+        //     tagline: 'Appointments. Invoices. Clients. More.',
+        //     image: '~/assets/calendar-mobile-laptop.png',
+        //     // backgroundImage: 'https://images.unsplash.com/photo-1517497052582-25e6fe8ec001?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a796c72fc4b68772c227b39dc6c3754&auto=format&fit=crop&w=1950&q=80'
+        //     // backgroundImage: 'https://images.unsplash.com/photo-1496096265110-f83ad7f96608?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=35840c5a386390076c95d47b745ae459&auto=format&fit=crop&w=1950&q=80'
+        //     // backgroundImage: 'https://images.unsplash.com/photo-1496167117681-944f702be1f4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8a9e41e445772471708b74b0835e4c35&auto=format&fit=crop&w=2378&q=80'
+        //     // backgroundImage: 'https://images.unsplash.com/photo-1485714104612-01bce6019729?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e63a299ccd3b0c5cd4149a4d06a2c9e8&auto=format&fit=crop&w=1993&q=80'
+        //     // backgroundImage: 'https://images.unsplash.com/photo-1501696461415-6bd6660c6742?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=defe121eb958a24b9e2c4f2041897339&auto=format&fit=crop&w=1868&q=80'
+        //     backgroundImage: 'https://images.unsplash.com/photo-1515136194762-84618729b8df?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f26f412a62f4e342b739989b9e8aa308&auto=format&fit=crop&w=1950&q=80'
+        //     // backgroundImage: 'https://images.unsplash.com/photo-1463134836706-8bcc60f7d78b?ixlib=rb-0.3.5&s=ae9b36a5009e585b400b2f5370ff9858&auto=format&fit=crop&w=1950&q=80'
+        //     //backgroundImage: 'https://images.unsplash.com/photo-1515386078358-daf436633fba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4acb1b2241487c257e65c113766b8e46&auto=format&fit=crop&w=1949&q=80'
+        //   }
+        // },
+        {
+          name: Callout,
+          props: {
+            callout:
+              "Integrate AppointmentGuru with Zoom so you can run your business online"
+          }
+        },
+        {
+          name: FeatureBox,
+          inContainer: true,
+          props: {
+            rowSize: 3,
+            features: [
+              {
+                icon: "videocam",
+                title: "Integrate with Zoom",
+                description:
+                  "Connect your AppointmentGuru account to your Zoom account so that you can manage your Zoom meetings right from AppointmentGuru"
+              },
+              {
+                icon: "flash_on",
+                title: "Automate your notifications",
+                description:
+                  "Once connected, AppointmentGuru can automatically send your Zoom meeting information to your clients with our automatated confirmations and reminders"
+              },
+              {
+                icon: "language",
+                title: "Let your clients book online",
+                description:
+                  "Your clients can book appointments online via your online booking page, our mobile app, or though a booking form embedded in your web page"
+              }
+            ]
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: "mt-0",
+            isLeft: true,
+            withBackground: true,
+            tagline: "More than just an appointment manager app",
+            content:
+              "Automate your appointment management, send good-looking invoices, enable online scheduling, be better at running your business, free up more time.",
+            img: "/img/schedule.png"
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: "mt-4",
+            tagline: "Manage your business on the go",
+            content:
+              "Keep up-to-date with your daily appointment maker and invoicing with the AppointmentGuru app.",
+            img: "/img/create_appointment.png"
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: "mt-4",
+            isLeft: true,
+            withBackground: true,
+            tagline: "Lessen no-shows with notifications",
+            content:
+              "Automatically send customisable SMS or email notifications to confirm, remind, reschedule and cancel appointments through your online appointment system.",
+            img: "/img/notification.png"
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: ["mt-4", "invoicing"],
+            tagline: "Built-in invoicing plus an integration with SnapScan",
+            content:
+              "Industry-standard invoices generated from your appointments that can include medical ICD-10, NAPPI and procedure codes. Add physical products and accept credit card payments with AppointmentGuru's SnapScan integration.",
+            img: "/img/invoice.png"
+          }
+        },
+        {
+          name: FeatureBlock,
+          props: {
+            classes: "mt-4",
+            isLeft: true,
+            withBackground: true,
+            tagline: "Empower and know your clients",
+            content:
+              "Clients can book and manage their appointments with the AppointmentGuru app. Data, notes and personal details are all safely stored on our platform.",
+            img: "/img/book_online.png"
+          }
+        },
+        {
+          name: Testimonial,
+          inContainer: true,
+          props: {
+            classes: "elevation-2",
+            quote:
+              "I don't think I could imagine running my business without AppointmentGuru anymore.",
+            authorName: "Sean Johnson",
+            authorBio: "Natural Therapist",
+            link: "http://bookme.guru/structuralmedicine/",
+            linkText: "Book now with the Centre for Structural Medicine",
+            image:
+              "https://media-appointmentguru.s3.amazonaws.com/practitioner/images/7/profile.png"
+          }
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+@media only screen and (min-width: 1904px) {
+  .container {
+    max-width: 1185px !important;
+  }
+}
+@media only screen and (max-width: 599px) {
+  .container {
+    padding: 0px;
+  }
+}
+.blockquote.smaller {
+  font-size: 16px;
+}
+</style>
